279388c776ce34ade2018eb4ad6fa7d0c95e5c1d
PelykhV <119511581+PelykhV@users.noreply.github.com>
2023-06-04 16:30:57 +0300

    Update hotel.py

--- hotel/models/hotel.py
+++ hotel/models/hotel.py
@@ -1,4 +1,9 @@
+"""
+The module that contains the Hotel class is an abstract class that represents a hotel.
+"""
 from abc import ABC, abstractmethod
+
+from exceptions import NoAvailableRoomsException, NegativeValueException
 
 
 class Hotel(ABC):
@@ -22,11 +27,10 @@ class Hotel(ABC):
         self.rating = rating
 
     @abstractmethod
-    def getLocation(self):
+    def get_location(self):
         """
         Abstract method to get the location information of the hotel.
         """
-        pass
 
     def book_room(self):
         """
@@ -34,6 +38,8 @@ class Hotel(ABC):
         """
         if self.available_rooms > 0:
             self.available_rooms -= 1
+        else:
+            raise NoAvailableRoomsException("No available rooms in the hotel.")
 
     def release_room(self):
         """
@@ -63,3 +69,41 @@ class Hotel(ABC):
         """
         return f"{self.__class__.__name__}(name={self.name}, total_rooms={self.total_rooms}, " \
                f"available_rooms={self.available_rooms}, rating={self.rating})"
+
+    def get_attributes_by_type(self, data_type):
+        """
+        Get a dictionary of attributes with their values for a specific data type.
+
+        Parameters:
+        data_type (type): The data type to filter the attributes.
+
+        Returns:
+        dict: Dictionary of attributes with their values for the specified data type.
+        """
+        return {key: value for key, value in self.__dict__.items() if isinstance(value, data_type)}
+
+    def update_rating(self, new_rating):
+        """
+        Update the rating of the hotel.
+
+        Args:
+            new_rating (float): The new rating of the hotel.
+
+        Raises:
+            NegativeValueException: If the new rating is a negative value.
+        """
+        if new_rating < 0:
+            raise NegativeValueException("Rating cannot be a negative value.")
+
+        self.rating = new_rating
+
+    __instance = None
+
+    @staticmethod
+    def get_instance():
+        """
+        Get instanceof the Hotel class
+        """
+        if not Hotel.__instance:
+            Hotel.__instance = Hotel()
+        return Hotel.__instance
