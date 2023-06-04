a60a5eb8f811ca88b236dc14bce3a61e2ca059b3
PelykhV <119511581+PelykhV@users.noreply.github.com>
2023-06-04 16:31:30 +0300

    Create exceptions.py

---
+++ hotel/exceptions.py
@@ -0,0 +1,22 @@
+class NegativeValueException(ValueError):
+    """
+    Exception raised when a field is assigned a negative value.
+
+    Attributes:
+        field_name (str): The name of the field with the negative value.
+    """
+
+    def __init__(self, field_name):
+        self.field_name = field_name
+
+    def __str__(self):
+        return f"Field {self.field_name} cannot have a negative value"
+
+
+class NoAvailableRoomsException(Exception):
+    """
+    Exception raised when there are no available rooms for booking.
+    """
+
+    def __str__(self):
+        return "No available rooms for booking"
