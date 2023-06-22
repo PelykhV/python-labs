56a9a8ab17bc76a93b0d72848035bc4aef9905d5
PelykhV <119511581+PelykhV@users.noreply.github.com>
2023-06-22 13:07:35 +0300

    Update esort_hotel.py

--- hotel/models/resort_hotel.py
+++ hotel/models/resort_hotel.py
@@ -1,13 +1,19 @@
-from models.Hotel import Hotel
+"""
+The module containing the ResortHotel class is a subclass of the Hotel class
+representing a resort hotel.
+"""
+from models.hotel import Hotel
 
 
+# pylint: disable=too-few-public-methods
 class ResortHotel(Hotel):
     """
     Represents a hotel in a resort.
     """
 
-    def __init__(self, name="", total_rooms=0, available_rooms=0, rating=0.0, resort_name="", num_restaurants=0,
-                 has_child_pool=False, has_adult_pool=False):
+    # pylint: disable=too-many-arguments
+    def __init__(self, name="", total_rooms=0, available_rooms=0, rating=0.0, resort_name="",
+                 num_restaurants=0, has_child_pool=False, has_adult_pool=False):
         """
         Creates a new instance of a ResortHotel.
 
@@ -27,8 +33,15 @@ class ResortHotel(Hotel):
         self.has_child_pool = has_child_pool
         self.has_adult_pool = has_adult_pool
 
-    def getLocation(self):
+    def get_location(self):
         """
         Returns the name of the resort.
         """
         return self.resort_name
+
+    @staticmethod
+    def do_something():
+        """
+        This method do something
+        """
+        return "Motel-specific action"
